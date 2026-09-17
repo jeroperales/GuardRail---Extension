0e84528c8d9f8266b0f2427f82fe645e6503cdbb
jero <jeronimoperales1@gmail.com>
2026-09-16 23:15:28 -0600

    Fixed mistake in CSS, created style CSS for general, started link input in Link Scanner Function

--- routes/linkScannerFunction/linkScannerFunction.js
+++ routes/linkScannerFunction/linkScannerFunction.js
@@ -1,6 +1,12 @@
 
 const button = document.getElementById('backButton');
 
+const input = document.getElementById('linkInput')  //RECIEVES FROM HTML LINK THAT WAS INPUTED
+const scanButton = document.getElementById('scanBtn') 
+const resultBox = document.getElementById('resultBox') //Sends feed back to HTML and updates the result box 
+const resultText = document.getElementById('resultText'); //
+
+
 button.addEventListener("click", () => {
     
   window.location.href = "/popup.html";
