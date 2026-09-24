c7c8850f99143527de4d971714157f3e7e66517c
jero <jeronimoperales1@gmail.com>
2026-09-23 22:51:58 -0600

    started virus total api, api request works, analysis no

--- virustotal-api/virustotal.py
+++ virustotal-api/virustotal.py
@@ -1,34 +1,50 @@
 import requests
 import os
+import codecs
 from dotenv import load_dotenv
 
 
 # Allows us to use Enviroment Varibles
 load_dotenv()
-apiKey = os.getenv("TOKENVIRUS") # CALLS THE TOKEN ENVIRONMENT
+apiKey = os.getenv("TOTALVIRUS_KEY") # CALLS THE TOKEN ENVIRONMENT
 AccountID = os.getenv("ACCOUNT_ID_VIRUSTOTAL")
 
 
-zipPassword = ''
 
-print (apiKey)
+with open('virustester.txt', 'rb') as fp:
+    v = fp.read()
+
+    print(f'\n \n \n \n ', { v }, '\n \n \n \n' )
+
+
+
+zipPassword = '' #potential password for zips
+
+
+
+
+#print (f'THIS IS THE API KEY BROTHER', {apiKey})
+
 
 url = "https://www.virustotal.com/api/v3/files"
 
-# files = { "file": ("", open("", "rb"), "") }
+
+files = { "file": ('virustester.txt', open('virustester.txt', "rb"), "") } 
+
+
 
 payload = { "password": zipPassword } #IF THE FILE HAS A PASSWORD IT WOULD PROCESS HERE)?
 
+
 headers = {
-    "x-apikey": {apiKey},
+    "x-apikey": apiKey,
     "accept": "application/json",
-    "content-type": "multipart/form-data"
+    
 }
 
-response = requests.post(url, headers=headers)
-
-
+response = requests.post(url, files=files, headers=headers)
 
 print(response.text)
+
 
 
